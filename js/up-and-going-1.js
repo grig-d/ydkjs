f572be956e19d2dfee51263ed89e34d1fe8bf285
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-02-23 03:56:37 +0200

    done

--- js/up-and-going-1.js
+++ js/up-and-going-1.js
@@ -1,1 +1,31 @@
-console.log('hi');
+const PHONE_PRICE = 199.99;
+const ACCESSORY_PRICE = 9.99;
+const SHOPPING_LIMIT = 250;
+const TAX_RATE = 0.15;
+
+const cardBalance = Number(prompt('Card Balance?'));
+let amount = 0;
+
+function tax(amount) {
+  return amount * TAX_RATE;
+}
+
+function formattedValue(value) {
+  return (value = '$' + value.toFixed(2));
+}
+
+while (amount + PHONE_PRICE < cardBalance) {
+  amount = amount + PHONE_PRICE;
+  console.log('Phone added to card');
+  while (amount + ACCESSORY_PRICE < SHOPPING_LIMIT) {
+    amount = amount + ACCESSORY_PRICE;
+    console.log('Accessory added to card');
+  }
+  amount = amount + tax(amount);
+}
+
+console.log('Total Amount: ', formattedValue(amount));
+
+if (amount > cardBalance || amount === 0) {
+  console.log('there are not enough funds in your account');
+}
